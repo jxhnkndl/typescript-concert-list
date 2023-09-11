03b2a5b598967f56400bfe116019f40ad4a0c922
jxhnkndl <jkroyston@gmail.com>
2023-09-11 12:10:44 -0400

    create placeholder header and footer components and configure main app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import styles from "./App.module.css";
+import Footer from "./layout/Footer/Footer"
+import Header from "./layout/Header/Header"
+
 function App() {
   return (
-    <div>
-      <h1>HELLO</h1>
+    <div className={styles.wrapper}>
+      <Header />
+      <main className={styles["main-content"]}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, sed, animi, reiciendis aperiam praesentium accusantium numquam a voluptatum ex culpa doloribus! In debitis blanditiis vero itaque? Cumque in, aperiam illo sit ad, nemo quae totam id architecto quos a ratione? Eaque sapiente dolorem incidunt deleniti id sunt. Eaque eligendi, vero incidunt molestiae quibusdam tempore facere quia voluptates, molestias, quidem maxime! Unde amet sed cupiditate fuga rerum aliquid commodi voluptate suscipit quibusdam corporis facere, iusto magni minus dignissimos aspernatur dolore dolor quod incidunt optio eaque. Praesentium, ex. Sunt cumque, aliquam sit porro velit iure optio asperiores quaerat harum atque quis quisquam non expedita voluptatem dicta molestiae necessitatibus! Modi deleniti in minus doloribus ea voluptas ex, obcaecati nostrum quibusdam quisquam nobis ab. Ea veritatis aperiam quam commodi magni iusto pariatur ipsam, laborum ad soluta possimus necessitatibus molestiae architecto impedit eum eaque. Id et laborum nisi optio rerum ullam accusantium dolor dolores veniam molestias ut odio error libero magnam accusamus tempora quis ipsum, natus aperiam. Corporis quod molestiae inventore blanditiis minus sed porro neque ea, quaerat eveniet assumenda ducimus aspernatur impedit voluptate, accusantium molestias? Incidunt neque doloribus, sapiente cum saepe laboriosam ut aliquid, ea cupiditate doloremque quia necessitatibus magnam? Cupiditate eum tempore architecto.
+      </main>
+      <Footer />
     </div>
   )
 }
---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h2>HEADER</h2>
+    </header>
+  )
+}
+
+export default Header
