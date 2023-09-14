cad45761bc2bc2f7f42163a415c2910b4f38d511
jxhnkndl <jkroyston@gmail.com>
2023-09-14 10:30:02 -0400

    tweak concert card styles

--- src/components/Concert/Concert.tsx
+++ src/components/Concert/Concert.tsx
@@ -3,15 +3,42 @@ import styles from './Concert.module.css';
 import { ConcertData } from '../../data';
 
 interface ConcertProps {
-  concert: ConcertData
+  concert: ConcertData;
 }
 
 function Concert({ concert }: ConcertProps) {
   return (
-    <div className={styles.concert}>
-      <p>CONCERT</p>
-    </div>
-  )
+    <article className={styles.concert}>
+      <div className={`${styles['image-col']} ${styles.column}`}>
+        <div
+          className={`${styles.image}`}
+          style={{
+            backgroundImage: `url(${concert.image})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'top',
+          }}
+        ></div>
+      </div>
+      <div className={`${styles['main-col']} ${styles.column}`}>
+        <div className={`${styles['details-container']}`}>
+          <div className={`${styles.details}`}>
+            <p className={`${styles.date}`}>{concert.date}</p>
+
+            <h2 className={`${styles.headliner}`}>{concert.headliner}</h2>
+            {concert.support && (
+              <p className={`${styles.support}`}>w/ {concert.support}</p>
+            )}
+            <p className={styles.venue}>{concert.venue}</p>
+            <p>Doors {concert.time}</p>
+          </div>
+          <div className={`${styles.buttons}`}>
+            <button className={`${styles.button}`}>Buy Tickets</button>
+            <p className={`${styles.price}`}>From ${concert.price}.00</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
 
-export default Concert
+export default Concert;
