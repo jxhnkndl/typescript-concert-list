5cf52793a1f73c91ee966a66054969da3201b3f0
jxhnkndl <jkroyston@gmail.com>
2023-09-18 11:52:32 -0400

    add link functionality to header component

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,11 +1,20 @@
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { TbCalendarPlus } from 'react-icons/tb';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.h1}>RVA Noise</h1>
+      <Link to="/" className={styles.link}>
+        <h1 className={styles.h1}>RVA Noise</h1>
+      </Link>
+      <nav className={styles.nav}>
+        <Link to="/create" className={styles.link}>
+          <TbCalendarPlus className={styles.icon} />
+        </Link>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
