7f5b06c8ffc1bfa3b9e2193c296b6836dd805005
jxhnkndl <jkroyston@gmail.com>
2023-09-21 11:41:44 -0400

    create animated form component container

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from './layout/Footer/Footer';
 import Header from './layout/Header/Header';
+import { useState } from 'react';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleForm = () => {
+    setIsOpen(prev => !prev);
+  }
+
   return (
     <div className={styles.wrapper}>
-      <Header />
+      <Header isOpen={isOpen} toggleForm={toggleForm} />
       <main className={styles['main-content']}>
         <Outlet />
       </main>
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,18 +1,43 @@
 import { Link } from 'react-router-dom';
+import Form from '../../components/Form/Form';
 import styles from './Header.module.css';
 
-const Header = () => {
+interface Props {
+  isOpen: boolean;
+  toggleForm: () => void;
+}
+
+const Header = ({ isOpen, toggleForm }: Props) => {
+  console.log(isOpen)
   return (
-    <header className={styles.header}>
-      <Link to="/" className={styles.link}>
-        <h1 className={styles.h1}>RVA Noise</h1>
-      </Link>
-      <nav className={styles.nav}>
-        <Link to="/create" className={styles.link}>
-          <button className={styles.btn}>+</button>
+    <>
+      <header className={styles.header}>
+        <Link to="/" className={styles.link}>
+          <h1 className={styles.h1}>RVA Noise</h1>
         </Link>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <Link
+            to="/create"
+            className={styles.link}
+            onClick={toggleForm}
+          >
+            <button className={styles.btn}>
+              <span className={`${styles.plus} ${isOpen ? styles.rotate : ''}`}>
+                +
+              </span>
+            </button>
+          </Link>
+        </nav>
+      </header>
+
+      <section
+        className={`${styles['form-container']} ${
+          isOpen ? styles.open : styles.closed
+        }`}
+      >
+        <Form />
+      </section>
+    </>
   );
 };
 
