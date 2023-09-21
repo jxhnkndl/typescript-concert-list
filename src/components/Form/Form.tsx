ef83cae00c56644cf621afece10f269630b095b9
jxhnkndl <jkroyston@gmail.com>
2023-09-21 12:21:04 -0400

    create form ui

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,9 +1,39 @@
 import styles from './Form.module.css';
 
+
+
 const Form = () => {
   return (
     <form className={styles.form}>
-      FORM
+      <h2 className={styles.h2}>Add Concert</h2>
+      <div className={styles['form-group']}>
+        <input className={styles.input} type="date" name="date" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='Venue' type="text" name="venue" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='City' type="text" name="city" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='State' type="text" name="state" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='Headliner' type="text" name="headliner" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='Support' type="text" name="support" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='Time' type="text" name="time" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='Price' type="text" name="price" />
+      </div>
+      <div className={styles['form-group']}>
+        <input className={styles.input} placeholder='Image URL' type="text" name="image" />
+      </div>
+      <button className={styles.btn}>Submit</button>
     </form>
   )
 }
